ecc96f7fbca66fd9d892043f6463f8b1f570a7ba
Admin <admin@admins-mbp.home>
2018-08-28 13:11:44 +0100

    setup webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, './src/js'),
+	output: {
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 8080
+	},
+	devtool: "source-map",
+	context: __dirname,
+	module: {
+		rules: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+			{ test: /\.scss$/, loader: [
+				'style-loader',
+				'css-loader',
+				'sass-loader'
+			]}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Spend a billion dollars',
+			filename: 'index.html',
+			template: path.resolve(__dirname, './src/index.html')
+		})
+	]
+};
